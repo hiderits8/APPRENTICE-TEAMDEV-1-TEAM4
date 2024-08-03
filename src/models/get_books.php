1d3494c5aeb1784a25b234ef8e17cab16ef670fe
HakuryuSato <r700540@gmail.com>
2024-08-03 20:36:08 +0900

    ファイル移動

--- src/models/get_books.php
+++
@@ -1,8 +0,0 @@
-<?php
-// データベース接続
-function fetchBooksFromDatabase() {
-    $pdo = new PDO('mysql:host=localhost;dbname=booksdb', 'user', 'password');
-    $stmt = $pdo->query('SELECT * FROM books');
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-?>
