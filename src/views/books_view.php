bace57d7e883891689df9807b7f22dcc1ebb62bc
HakuryuSato <r700540@gmail.com>
2024-08-03 20:36:40 +0900

    本の外観表示用

--- src/views/books_view.php
+++ src/views/books_view.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Books</title>
+    <title>Books Data</title>
 </head>
 <body>
+    <h1>Books Data</h1>
     <div id="books-data"></div>
-    <script src="../js/fetch_books.js"></script>
-
+    <script src="../public/js/fetch_books.js"></script>
 </body>
 </html>
