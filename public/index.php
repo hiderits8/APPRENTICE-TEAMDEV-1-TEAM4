9cfb27e17e933236280022b696ee75fd0cf484df
HakuryuSato <r700540@gmail.com>
2024-08-03 20:25:40 +0900

    books_viewを呼び出し

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
 
-$text = "Hello_world";
-echo $text;
+// メインページ、ここに対して様々なviewを呼びだし、
+// JavaScriptで表示非表示を切り替え、SPAを実現する壮大な計画が立てられている。
+
+// viewsから、Booksのビューを呼び出し
+include '../src/views/books_view.php';
+
